dbb7da84874ccd33398a734ad6a94d82b4acef59
Pavel Asyutchenko <sventeam@yandex.ru>
2020-06-21 03:14:55 +0300

    Changed setting kIsDebugBuild/kIsReleaseBuild from using _NDEBUG to a more robust approach with CMake build type

--- include/voxen/config.hpp.cmake
+++ include/voxen/config.hpp.cmake
@@ -11,12 +11,12 @@ public:
 	constexpr static const char kVersionSuffix[] = "@VOXEN_VERSION_SUFFIX@";
 	constexpr static const char kVersionString[] = "@VOXEN_VERSION_STRING@";
 
-#ifdef _NDEBUG
-	constexpr static bool kIsDebugBuild = false;
-	constexpr static bool kIsReleaseBuild = true;
-#else
+#if VOXEN_DEBUG_BUILD == 1
 	constexpr static bool kIsDebugBuild = true;
 	constexpr static bool kIsReleaseBuild = false;
+#else
+	constexpr static bool kIsDebugBuild = false;
+	constexpr static bool kIsReleaseBuild = true;
 #endif
 	constexpr static bool kUseVulkanDebugging = @USE_VULKAN_DEBUGGING@;
 	constexpr static bool kUseIntegratedGpu = @USE_INTEGRATED_GPU@;
