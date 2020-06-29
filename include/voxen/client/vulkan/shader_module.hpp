20f13f16c4b100cdb6e748b0d181deea3cccea38
Pavel Asyutchenko <sventeam@yandex.ru>
2020-06-29 23:10:51 +0300

    Added VulkanShaderModuleCollection

--- include/voxen/client/vulkan/shader_module.hpp
+++ include/voxen/client/vulkan/shader_module.hpp
@@ -19,4 +19,20 @@ private:
 	VkShaderModule m_shader_module = VK_NULL_HANDLE;
 };
 
+class VulkanShaderModuleCollection {
+public:
+	VulkanShaderModuleCollection();
+	VulkanShaderModuleCollection(VulkanShaderModuleCollection &&) = delete;
+	VulkanShaderModuleCollection(const VulkanShaderModuleCollection &) = delete;
+	VulkanShaderModuleCollection &operator = (VulkanShaderModuleCollection &&) = delete;
+	VulkanShaderModuleCollection &operator = (const VulkanShaderModuleCollection &) = delete;
+	~VulkanShaderModuleCollection() = default;
+
+	VulkanShaderModule &debugOctreeVertexShader() noexcept { return m_debug_octree_vertex; }
+	VulkanShaderModule &debugOctreeFragmentShader() noexcept { return m_debug_octree_fragment; }
+private:
+	VulkanShaderModule m_debug_octree_vertex;
+	VulkanShaderModule m_debug_octree_fragment;
+};
+
 }
