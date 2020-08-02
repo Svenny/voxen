31aef34dca8c49f071aacfb154e54dfd59a59416
Pavel Asyutchenko <sventeam@yandex.ru>
2020-08-02 22:19:39 +0300

    Added ability for vulkan::Backend to restore after surface loss/swapchain out-of-date

--- src/client/render.cpp
+++ src/client/render.cpp
@@ -3,25 +3,56 @@
 #include <voxen/client/vulkan/high/main_loop.hpp>
 #include <voxen/client/vulkan/backend.hpp>
 
+#include <voxen/util/assert.hpp>
 #include <voxen/util/exception.hpp>
 #include <voxen/util/log.hpp>
 
 namespace voxen::client
 {
 
-Render::Render(Window &window) {
-	if (!vulkan::Backend::backend().start(window)) {
+Render::Render(Window &window)
+	: m_window(window)
+{
+	if (!vulkan::Backend::backend().start(m_window)) {
 		Log::error("Render subsystem couldn't launch");
 		throw MessageException("failed to start render subsystem");
 	}
 }
 
-Render::~Render() {
+Render::~Render()
+{
 	vulkan::Backend::backend().stop();
 }
 
-void Render::drawFrame(const World &state, const GameView &view) {
-	vulkan::Backend::backend().mainLoop()->drawFrame(state, view);
+void Render::drawFrame(const World &state, const GameView &view)
+{
+	auto &backend = vulkan::Backend::backend();
+	if (!backend.drawFrame(state, view)) {
+		auto state = backend.state();
+
+		if (state == vulkan::Backend::State::Broken) {
+			Log::error("Render subsystem encountered a non-recoverable error, shutting down");
+			throw MessageException("render subsystem failure");
+		}
+		else if (state == vulkan::Backend::State::SurfaceLost) {
+			Log::info("Trying to recreate surface...");
+			if (!backend.recreateSurface(m_window)) {
+				Log::error("Surface recreation failed, shutting down");
+				throw MessageException("failed to recreate surface");
+			}
+		}
+		else if (state == vulkan::Backend::State::SwapchainOutOfDate) {
+			Log::info("Trying to recreate swapchain...");
+			if (!backend.recreateSwapchain(m_window)) {
+				Log::error("Swapchain recreation failed, shutting down");
+				throw MessageException("failed to recreate swapchain");
+			}
+		}
+		else {
+			// This branch must be unreachable
+			vxAssertStrong(false);
+		}
+	}
 }
 
 }
