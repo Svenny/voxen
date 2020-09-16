df0f37689e0f5b71dbe0a8645eba139f4ef9b963
Pavel Asyutchenko <sventeam@yandex.ru>
2020-09-16 21:27:12 +0300

    Added FNV-1a hash to utils. Added stub for TerrainSeam.

--- src/common/terrain/chunk.cpp
+++ src/common/terrain/chunk.cpp
@@ -1,5 +1,7 @@
 #include <voxen/common/terrain/chunk.hpp>
 
+#include <voxen/util/hash.hpp>
+
 #include <algorithm>
 #include <array>
 #include <cstring>
@@ -16,26 +18,18 @@ bool TerrainChunkHeader::operator== (const TerrainChunkHeader &other) const noex
 uint64_t TerrainChunkHeader::hash() const noexcept
 {
 #pragma pack(push, 1)
-	union {
-		struct {
-			uint64_t u64[3];
-			uint32_t u32;
-		} data;
-		uint8_t bytes[sizeof(data)];
-	} conv;
+	struct {
+		uint64_t u64[3];
+		uint32_t u32;
+	} data;
 #pragma pack(pop)
 
-	conv.data.u64[0] = static_cast<uint64_t>(base_x);
-	conv.data.u64[1] = static_cast<uint64_t>(base_y);
-	conv.data.u64[2] = static_cast<uint64_t>(base_z);
-	conv.data.u32 = (scale);
-	// FNV-1a
-	uint64_t result = 0xCBF29CE484222325;
-	for (size_t i = 0; i < std::size(conv.bytes); i++) {
-		result *= 0x100000001B3;
-		result ^= uint64_t(conv.bytes[i]);
-	}
-	return result;
+	data.u64[0] = static_cast<uint64_t>(base_x);
+	data.u64[1] = static_cast<uint64_t>(base_y);
+	data.u64[2] = static_cast<uint64_t>(base_z);
+	data.u32 = (scale);
+
+	return hashFnv1a(&data, sizeof(data));
 }
 
 TerrainChunk::TerrainChunk(const TerrainChunkCreateInfo &info)
---
+++ src/util/hash.cpp
@@ -0,0 +1,17 @@
+#include <voxen/util/hash.hpp>
+
+namespace voxen
+{
+
+uint64_t hashFnv1a(const void *data, std::size_t size) noexcept
+{
+	const uint8_t *bytes = reinterpret_cast<const uint8_t *>(data);
+	uint64_t result = 0xCBF29CE484222325;
+	for (std::size_t i = 0; i < size; i++) {
+		result *= 0x100000001B3;
+		result ^= uint64_t(bytes[i]);
+	}
+	return result;
+}
+
+}
