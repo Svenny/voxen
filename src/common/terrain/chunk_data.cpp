10aae0b29fe94054a6d8a6d4c574a43cceec945f
Pavel Asyutchenko <sventeam@yandex.ru>
2020-09-30 21:52:21 +0300

    Added TerrainChunkOctree and TerrainChunkPrimaryData::materialsOfCell

--- src/common/terrain/chunk_data.cpp
+++ src/common/terrain/chunk_data.cpp
@@ -3,5 +3,26 @@
 namespace voxen
 {
 
+std::array<voxel_t, 8> TerrainChunkPrimaryData::materialsOfCell(glm::uvec3 cell) const noexcept
+{
+	// Voxels data - YXZ storage
+	const uint32_t y = cell.y;
+	const uint32_t x = cell.x;
+	const uint32_t z = cell.z;
+	assert(x < GRID_CELL_COUNT);
+	assert(y < GRID_CELL_COUNT);
+	assert(z < GRID_CELL_COUNT);
+
+	std::array<voxel_t, 8> mats;
+	mats[0] = voxels[y][x][z];
+	mats[1] = voxels[y][x][z+1];
+	mats[2] = voxels[y][x+1][z];
+	mats[3] = voxels[y][x+1][z+1];
+	mats[4] = voxels[y+1][x][z];
+	mats[5] = voxels[y+1][x][z+1];
+	mats[6] = voxels[y+1][x+1][z];
+	mats[7] = voxels[y+1][x+1][z+1];
+	return mats;
+}
 
 }
