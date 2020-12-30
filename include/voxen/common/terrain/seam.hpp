8fb306e2aa432ca176330960f9d1efae3d201b57
Pavel Asyutchenko <sventeam@yandex.ru>
2020-12-31 01:24:23 +0300

    Optimized 'TerrainChunkSeamSet' node copying mechanism by using copy strategy mask instead of function pointers. Results of measurements on Intel i5-9300H: - Measure generating seam refs set (top-level `seamCellProc()` call) and octree extension (sum of all `extendOctree()` calls) in `TerrainOctree` on currently used world settings. Average over 12 measurements. - Old method provides min/avg/max of 6.39/6.725/8.43 ms - New method provides min/avg/max of 6.15/6.575/7.96 ms - Not much difference in times, but new method also decreases memory usage (less `std::vector`s, more tight data packing)

--- include/voxen/common/terrain/seam.hpp
+++ include/voxen/common/terrain/seam.hpp
@@ -17,31 +17,34 @@ public:
 	TerrainChunkSeamSet &operator = (const TerrainChunkSeamSet &) = default;
 	~TerrainChunkSeamSet() = default;
 
-	void addEdgeRef(const TerrainChunk *ptr, int dim) noexcept
+	template<int D> void addEdgeRef(const TerrainChunk *ptr) noexcept
 	{
 		assert(ptr);
-		m_edge_refs[dim].emplace_back(ptr, ptr->version());
+		uintptr_t p = reinterpret_cast<uintptr_t>(ptr) | uintptr_t(COPY_STRATEGY_MASK_EDGE[D]);
+		m_refs.emplace_back(p);
 	}
 
-	void addFaceRef(const TerrainChunk *ptr, int dim) noexcept
+	template<int D> void addFaceRef(const TerrainChunk *ptr) noexcept
 	{
 		assert(ptr);
-		m_face_refs[dim].emplace_back(ptr, ptr->version());
+		uintptr_t p = reinterpret_cast<uintptr_t>(ptr) | uintptr_t(COPY_STRATEGY_MASK_FACE[D]);
+		m_refs.emplace_back(p);
 	}
 
 	void clear() noexcept;
 
 	void extendOctree(TerrainChunkHeader header, ChunkOctree &output);
 
-	bool operator == (const TerrainChunkSeamSet &other) const noexcept;
-
 private:
-	using ChunkRef = std::pair<const TerrainChunk *, uint32_t>;
-
-	std::vector<ChunkRef> m_edge_refs[3];
-	std::vector<ChunkRef> m_face_refs[3];
+	// Three least significant bits of these pointers are
+	// set to the node copy strategy mask denoting which
+	// dimensions must be equal to the contact point
+	std::vector<uintptr_t> m_refs;
 
 	std::tuple<uint32_t, int64_t, int64_t, int64_t> selectExtendedRootDimensions(const TerrainChunkHeader &header) const;
+
+	static constexpr inline uint8_t COPY_STRATEGY_MASK_EDGE[3] = { 0b110, 0b101, 0b011 };
+	static constexpr inline uint8_t COPY_STRATEGY_MASK_FACE[3] = { 0b001, 0b010, 0b100 };
 };
 
 }
