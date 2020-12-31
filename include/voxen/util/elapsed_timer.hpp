748a671706b59d19d0623c1b73eff638f6b4dee0
Nikita Sirgienko <warquark@gmail.com>
2020-12-31 16:20:25 +0300

    Add useful util class - ElapsedTimer. This class can be used for code segment measurements with minimum code adding.

---
+++ include/voxen/util/elapsed_timer.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <voxen/config.hpp>
+
+#include <fmt/format.h>
+
+namespace voxen
+{
+
+/**
+ * This is class is voxen-specific class for code segment measurements.
+ * The class will collect time point on constructor/stop method and print them into log (Debug level) automatically.
+ * Should be used like this:
+ *    ElapsedTimer test_timer("section name");
+ *    <measurable code here>
+ *    test_timer.stop()
+ */ 
+class ElapsedTimer {
+public:
+    ElapsedTimer(std::string section_name, std::string format = "[Elapsed timer] execution of section \"{}\" tooks {} ms");
+    ElapsedTimer(const ElapsedTimer& other) = delete;
+    ElapsedTimer(ElapsedTimer&& other) = delete;
+    ~ElapsedTimer() noexcept;
+
+    ElapsedTimer& operator=(const ElapsedTimer& other) = delete;
+    ElapsedTimer& operator=(ElapsedTimer&& other) = delete;
+
+    void stop();
+
+private:
+    const std::string m_section_name;
+    const std::string m_format;
+
+    bool m_finished = false;
+    std::chrono::time_point<std::chrono::high_resolution_clock> m_start;
+    std::chrono::time_point<std::chrono::high_resolution_clock> m_end;
+};
+
+}
---
+++ src/util/elapsed_timer.cpp
@@ -0,0 +1,39 @@
+#include <voxen/util/elapsed_timer.hpp>
+
+#include <voxen/util/log.hpp>
+
+#include <chrono>
+#include <cassert>
+
+namespace voxen
+{
+
+ElapsedTimer::ElapsedTimer(std::string section_name, std::string format): m_section_name(std::move(section_name)), m_format(std::move(format))
+{
+    // ElapsedTimer for dev propouse only, so this shouldn't be used in Deploy Builds
+    if constexpr (BuildConfig::kIsDeployBuildTest) {
+        Log::fatal("Elapsed timers mustn't used in deploy builds, but timer for \"{}\" have been created", m_section_name);
+    }
+
+    m_start = std::chrono::high_resolution_clock::now();
+}
+
+ElapsedTimer::~ElapsedTimer() noexcept
+{
+    if (!m_finished) {
+        Log::warn("Elapsed timer for section \"{}\" destroyed without stop!", m_section_name);
+    }
+}
+
+void ElapsedTimer::stop()
+{
+    m_end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> diff = m_end - m_start;
+    std::chrono::milliseconds elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(diff);
+    Log::debug(m_format, m_section_name, elapsed_ms.count());
+
+    m_finished = true;
+}
+
+}
