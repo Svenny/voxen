18313ded787ba9e387c3e43ab4fa1ee0f3608d12
Pavel Asyutchenko <sventeam@yandex.ru>
2021-05-17 21:56:54 +0300

    [extras] Added 'spinlock' class

---
+++ include/extras/spinlock.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <atomic>
+
+namespace extras
+{
+
+// A lightweight alternative for `std::mutex`, may be used for protecting
+// short critical sections with small amount of expected thread contention
+class spinlock final {
+public:
+	spinlock() = default;
+	spinlock(spinlock &&) = delete;
+	spinlock(const spinlock &) = delete;
+	spinlock &operator = (spinlock &&) = delete;
+	spinlock &operator = (const spinlock &) = delete;
+	~spinlock() = default;
+
+	void lock() noexcept;
+	bool try_lock() noexcept;
+	void unlock() noexcept;
+
+private:
+	std::atomic_bool m_payload;
+};
+
+}
