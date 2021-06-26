03ce4c1c18323817d7ccdf49d6a4b9c5ebd2268d
Pavel Asyutchenko <sventeam@yandex.ru>
2021-06-27 00:19:26 +0300

    [terrain] Rewritten 'TerrainChunkSurface' to new terrain arch

--- include/voxen/common/terrain/surface.hpp
+++ include/voxen/common/terrain/surface.hpp
@@ -1,58 +1,115 @@
 #pragma once
 
-#include <glm/glm.hpp>
+#include <voxen/util/aabb.hpp>
+
+#include <extras/refcnt_ptr.hpp>
+
+#include <glm/vec3.hpp>
 
 #include <vector>
 
-namespace voxen
+namespace voxen::terrain
 {
 
-struct TerrainSurfaceVertex {
-	// Position in mesh-local coordinates
+struct SurfaceVertex final {
+	// Position in chunk-local coordinates
 	glm::vec3 position;
 	// Need not be normalized (despite its name lol)
 	glm::vec3 normal;
 };
 
-class TerrainSurface {
+// A base class for `ChunkOwnSurface` and `ChunkSeamSurface` for
+// reusing common code. Not intended to be used on its own.
+class ChunkSurfaceBase {
 public:
-	TerrainSurface() = default;
-	explicit TerrainSurface(size_t reserve_vertices, size_t reserve_indices);
-	TerrainSurface(TerrainSurface &&) = default;
-	TerrainSurface(const TerrainSurface &) = default;
-	TerrainSurface &operator = (TerrainSurface &&) = default;
-	TerrainSurface &operator = (const TerrainSurface &) = default;
-	~TerrainSurface() = default;
-
-	void clear() noexcept;
-
-	// Add an entry to the end of vertex array and return its index
-	uint32_t addVertex(const TerrainSurfaceVertex &vertex);
-	// Add three indices making a triangle to the end of index array
-	void addTriangle(uint32_t a, uint32_t b, uint32_t c);
-
-	// Vertex array size is guaranteed to never exceed UINT32_MAX
-	uint32_t numVertices() const noexcept { return static_cast<uint32_t>(m_vertices.size()); }
-	const TerrainSurfaceVertex *vertices() const noexcept { return m_vertices.data(); }
-	TerrainSurfaceVertex *vertices() noexcept { return m_vertices.data(); }
-
 	// Index array size is guaranteed to never exceed UINT32_MAX
 	uint32_t numIndices() const noexcept { return static_cast<uint32_t>(m_indices.size()); }
 	const uint32_t *indices() const noexcept { return m_indices.data(); }
 
 	// NOTE: AABB is calculated in mesh-local coordinates and is calculated
-	// by vertices, not triangles (so unused vertices contribute to AABB)
+	// by vertices, not triangles (so unused vertices also contribute to AABB)
+	const Aabb &aabb() const noexcept { return m_aabb; }
 
-	// Minimal (X,Y,Z) local-space coordinates of AABB
-	const glm::vec3 &aabbMin() const noexcept { return m_aabb_min; }
-	// Maximal (X,Y,Z) local-space coordinates of AABB
-	const glm::vec3 &aabbMax() const noexcept { return m_aabb_max; }
+protected:
+	std::vector<SurfaceVertex> m_vertices;
+	std::vector<uint32_t> m_indices;
+	Aabb m_aabb;
+
+	// This class is not intended for direct use, so ctors are not public
+	ChunkSurfaceBase() = default;
+	ChunkSurfaceBase(ChunkSurfaceBase &&) = default;
+	ChunkSurfaceBase(const ChunkSurfaceBase &) = default;
+	ChunkSurfaceBase &operator = (ChunkSurfaceBase &&) = default;
+	ChunkSurfaceBase &operator = (const ChunkSurfaceBase &) = default;
+	~ChunkSurfaceBase() = default;
+
+	void doClear() noexcept;
+	uint32_t doAddVertex(const SurfaceVertex &vertex);
+	void doAddTriangle(uint32_t a, uint32_t b, uint32_t c);
+};
+
+class ChunkOwnSurface final : public ChunkSurfaceBase {
+public:
+	ChunkOwnSurface() = default;
+	ChunkOwnSurface(ChunkOwnSurface &&) = default;
+	ChunkOwnSurface(const ChunkOwnSurface &) = default;
+	ChunkOwnSurface &operator = (ChunkOwnSurface &&) = default;
+	ChunkOwnSurface &operator = (const ChunkOwnSurface &) = default;
+	~ChunkOwnSurface() = default;
+
+	// Remove all added vertices and indices and reset AABB
+	void clear() noexcept { doClear(); }
+	// Add an entry to the end of vertex array and return its index
+	uint32_t addVertex(const SurfaceVertex &vertex) { return doAddVertex(vertex); }
+	// Add three indices making a triangle to the end of index array
+	void addTriangle(uint32_t a, uint32_t b, uint32_t c);
+
+	// Vertex array size is guaranteed to never exceed UINT32_MAX
+	uint32_t numVertices() const noexcept { return static_cast<uint32_t>(m_vertices.size()); }
+	const SurfaceVertex *vertices() const noexcept { return m_vertices.data(); }
+};
+
+class ChunkSeamSurface final : public ChunkSurfaceBase {
+public:
+	ChunkSeamSurface() = default;
+	ChunkSeamSurface(ChunkSeamSurface &&) = default;
+	ChunkSeamSurface(const ChunkSeamSurface &) = default;
+	ChunkSeamSurface &operator = (ChunkSeamSurface &&) = default;
+	ChunkSeamSurface &operator = (const ChunkSeamSurface &) = default;
+	~ChunkSeamSurface() = default;
+
+	// Set pointer to the "base (own) surface" for this seam surface. This operation acquires a reference.
+	// NOTE: vertices and indices are not cleared, use `clear()` before `init()` if needed.
+	// WARNING: base surface must always stay immutable until it's changed, index counting relies on it.
+	void init(extras::refcnt_ptr<ChunkOwnSurface> base_surface) noexcept;
+	// Remove all vertices, indices and reset AABB and base surface pointer
+	void clear() noexcept;
+
+	// Add an entry to the end of vertex array and return its index.
+	// NOTE: indexing is including vertices from base surface.
+	uint32_t addExtraVertex(const SurfaceVertex &vertex);
+	// Add three indices making a triangle to the end of index array.
+	// NOTE: indexing is including vertices from base surface.
+	void addTriange(uint32_t a, uint32_t b, uint32_t c);
+
+	// Returns the sum of numbers of vertices in own and seam surface
+	uint32_t numAllVertices() const noexcept;
+
+	// Vertex array size is guaranteed to never exceed UINT32_MAX
+	uint32_t numExtraVertices() const noexcept { return static_cast<uint32_t>(m_vertices.size()); }
+	const SurfaceVertex *extraVertices() const noexcept { return m_vertices.data(); }
 
 private:
-	std::vector<TerrainSurfaceVertex> m_vertices;
-	std::vector<uint32_t> m_indices;
-	glm::vec3 m_aabb_min { FLT_MAX, FLT_MAX, FLT_MAX };
-	glm::vec3 m_aabb_max { FLT_MIN, FLT_MIN, FLT_MIN };
+	extras::refcnt_ptr<ChunkOwnSurface> m_base_surface;
 };
+
+}
+
+namespace voxen
+{
+
+// TODO: remove these deprecated aliases
+using TerrainSurfaceVertex = terrain::SurfaceVertex;
+using TerrainSurface = terrain::ChunkOwnSurface;
 
 }
--- src/common/terrain/surface.cpp
+++ src/common/terrain/surface.cpp
@@ -3,38 +3,66 @@
 #include <cassert>
 #include <limits>
 
-namespace voxen
+namespace voxen::terrain
 {
 
-TerrainSurface::TerrainSurface(size_t reserve_vertices, size_t reserve_indices)
-{
-	m_vertices.reserve(reserve_vertices);
-	m_indices.reserve(reserve_indices);
-}
-
-void TerrainSurface::clear() noexcept
+void ChunkSurfaceBase::doClear() noexcept
 {
 	m_vertices.clear();
 	m_indices.clear();
-	m_aabb_min = { FLT_MAX, FLT_MAX, FLT_MAX };
-	m_aabb_max = { FLT_MIN, FLT_MIN, FLT_MIN };
+	m_aabb = Aabb();
 }
 
-uint32_t TerrainSurface::addVertex(const TerrainSurfaceVertex &vertex)
+uint32_t ChunkSurfaceBase::doAddVertex(const SurfaceVertex &vertex)
 {
-	assert(m_vertices.size() <= std::numeric_limits<uint32_t>::max());
 	m_vertices.emplace_back(vertex);
-	m_aabb_min = glm::min(m_aabb_min, vertex.position);
-	m_aabb_max = glm::max(m_aabb_max, vertex.position);
+	m_aabb.includePoint(vertex.position);
 	return static_cast<uint32_t>(m_vertices.size() - 1);
 }
 
-void TerrainSurface::addTriangle(uint32_t a, uint32_t b, uint32_t c)
+void ChunkSurfaceBase::doAddTriangle(uint32_t a, uint32_t b, uint32_t c)
 {
-	assert(std::max({ a, b, c }) < m_vertices.size());
 	m_indices.emplace_back(a);
 	m_indices.emplace_back(b);
 	m_indices.emplace_back(c);
+}
+
+void ChunkOwnSurface::addTriangle(uint32_t a, uint32_t b, uint32_t c)
+{
+	assert(std::max({ a, b, c }) < numVertices());
+	doAddTriangle(a, b, c);
+}
+
+void ChunkSeamSurface::init(extras::refcnt_ptr<ChunkOwnSurface> base_surface) noexcept
+{
+	assert(base_surface);
+	m_base_surface = std::move(base_surface);
+	m_aabb = m_base_surface->aabb();
+}
+
+void ChunkSeamSurface::clear() noexcept
+{
+	doClear();
+	m_base_surface.reset();
+}
+
+uint32_t ChunkSeamSurface::addExtraVertex(const SurfaceVertex &vertex)
+{
+	assert(m_base_surface);
+	uint32_t id = doAddVertex(vertex);
+	return id + m_base_surface->numVertices();
+}
+
+void ChunkSeamSurface::addTriange(uint32_t a, uint32_t b, uint32_t c)
+{
+	assert(std::max({ a, b, c }) < numAllVertices());
+	doAddTriangle(a, b, c);
+}
+
+uint32_t ChunkSeamSurface::numAllVertices() const noexcept
+{
+	assert(m_base_surface);
+	return m_base_surface->numVertices() + numExtraVertices();
 }
 
 }
