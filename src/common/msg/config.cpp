77ce300ed214d77bc070678f33273ee565f91397
Pavel Asyutchenko <sventeam@yandex.ru>
2022-02-24 00:31:05 +0300

    [msg] Added Config class

---
+++ src/common/msg/config.cpp
@@ -0,0 +1,12 @@
+#include <voxen/common/msg/config.hpp>
+
+namespace voxen::msg
+{
+
+// This file only contains some sanity checks of `Config` fields
+
+static_assert(Config::MESSAGE_SIZE > sizeof(void *), "Message size is close to useless");
+static_assert(Config::NUM_QUEUE_SHARDS > 0, "Queue must have at least one shard");
+static_assert(Config::QUEUE_SEGMENT_SIZE > 0, "Queue segment must hold at least one message");
+
+}
