ab0b36149ed4c2347c721734ec0ff49f97a0bd57
Pavel Asyutchenko <sventeam@yandex.ru>
2022-02-25 23:04:32 +0300

    [msg] Added Message class

---
+++ src/common/msg/message.cpp
@@ -0,0 +1,11 @@
+#include <voxen/common/msg/message.hpp>
+
+namespace voxen::msg
+{
+
+// Message payloads are close to useless if they can't even hold a pointer
+static_assert(sizeof(MessagePayloadStore) >= sizeof(void *), "Payload store is too small");
+static_assert(sizeof(Message) == Config::MESSAGE_SIZE, "MESSAGE_SIZE is not respected");
+static_assert(std::is_trivially_copyable_v<Message>, "Message type must be trivially copyable");
+
+}
