7fc648281694675f56f2303b9d2ca14c45099a0d
Pavel Asyutchenko <sventeam@yandex.ru>
2022-08-07 02:03:03 +0300

    [3rdparty] Converted fmt to new build system scripts

--- 3rdparty/fmt.cmake
+++ 3rdparty/fmt.cmake
@@ -1,5 +1,5 @@
-add_library(fmt STATIC "")
-add_library(fmt::fmt ALIAS fmt)
+voxen_add_library(fmt STATIC)
+add_library(3rdparty::fmt ALIAS fmt)
 
 target_include_directories(fmt PUBLIC fmt/include)
 target_sources(fmt PRIVATE fmt/src/format.cc fmt/src/os.cc)
