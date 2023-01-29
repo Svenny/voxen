037f01a01647a1bacafb1b1e7eaeda4568fe871a
Pavel Asyutchenko <sventeam@yandex.ru>
2023-01-29 21:50:11 +0300

    [vulkan] Improved getVk*String
    - Use string_view instead of raw pointers
    - Use semi-automatic VkResult literal generation (similar to VkFormat)
    - Protect VkResult case coverage by -Wswitch
    - Drop getVkResultDescription - costs maintenance and isn't very useful

--- src/client/vulkan/common.cpp
+++ src/client/vulkan/common.cpp
@@ -28,14 +28,13 @@ VulkanException::VulkanException(VkResult result, const char *api, extras::sourc
 	: Exception(loc), m_result(result)
 {
 	try {
-		const char *err = VulkanUtils::getVkResultString(result);
-		const char *desc = VulkanUtils::getVkResultDescription(result);
+		std::string_view err = VulkanUtils::getVkResultString(result);
 		if (api) {
 			Log::error("{} failed with error code {}", api, err, loc);
-			m_message = fmt::format("{} failed: {} ({})", api, err, desc);
+			m_message = fmt::format("{} failed: {}", api, err);
 		} else {
 			Log::error("Vulkan API call failed with error code {}", err, loc);
-			m_message = fmt::format("Vulkan error: {} ({})", err, desc);
+			m_message = fmt::format("Vulkan error: {}", err);
 		}
 	} catch (...) {
 		m_exception_occured = true;
@@ -145,115 +144,67 @@ const VkAllocationCallbacks *HostAllocator::callbacks() noexcept
 	return nullptr;
 }
 
-const char *VulkanUtils::getVkResultString(VkResult result) noexcept
+std::string_view VulkanUtils::getVkResultString(VkResult result) noexcept
 {
-	// Copy-pasted from Vulkan spec 1.2.132
+	using namespace std::string_view_literals;
+
+#define CASE(err) case err: return #err##sv
 	switch (result) {
 	// Result codes
-	case VK_SUCCESS: return "VK_SUCCESS";
-	case VK_NOT_READY: return "VK_NOT_READY";
-	case VK_TIMEOUT: return "VK_TIMEOUT";
-	case VK_EVENT_SET: return "VK_EVENT_SET";
-	case VK_EVENT_RESET: return "VK_EVENT_RESET";
-	case VK_INCOMPLETE: return "VK_INCOMPLETE";
-	case VK_SUBOPTIMAL_KHR: return "VK_SUBOPTIMAL_KHR";
+	CASE(VK_SUCCESS);
+	CASE(VK_NOT_READY);
+	CASE(VK_TIMEOUT);
+	CASE(VK_EVENT_SET);
+	CASE(VK_EVENT_RESET);
+	CASE(VK_INCOMPLETE);
+	CASE(VK_SUBOPTIMAL_KHR);
+	CASE(VK_THREAD_IDLE_KHR);
+	CASE(VK_THREAD_DONE_KHR);
+	CASE(VK_OPERATION_DEFERRED_KHR);
+	CASE(VK_OPERATION_NOT_DEFERRED_KHR);
+	CASE(VK_PIPELINE_COMPILE_REQUIRED);
 	// Error codes
-	case VK_ERROR_OUT_OF_HOST_MEMORY: return "VK_ERROR_OUT_OF_HOST_MEMORY";
-	case VK_ERROR_OUT_OF_DEVICE_MEMORY: return "VK_ERROR_OUT_OF_DEVICE_MEMORY";
-	case VK_ERROR_INITIALIZATION_FAILED: return "VK_ERROR_INITIALIZATION_FAILED";
-	case VK_ERROR_DEVICE_LOST: return "VK_ERROR_DEVICE_LOST";
-	case VK_ERROR_MEMORY_MAP_FAILED: return "VK_ERROR_MEMORY_MAP_FAILED";
-	case VK_ERROR_LAYER_NOT_PRESENT: return "VK_ERROR_LAYER_NOT_PRESENT";
-	case VK_ERROR_EXTENSION_NOT_PRESENT: return "VK_ERROR_EXTENSION_NOT_PRESENT";
-	case VK_ERROR_FEATURE_NOT_PRESENT: return "VK_ERROR_FEATURE_NOT_PRESENT";
-	case VK_ERROR_INCOMPATIBLE_DRIVER: return "VK_ERROR_INCOMPATIBLE_DRIVER";
-	case VK_ERROR_TOO_MANY_OBJECTS: return "VK_ERROR_TOO_MANY_OBJECTS";
-	case VK_ERROR_FORMAT_NOT_SUPPORTED: return "VK_ERROR_FORMAT_NOT_SUPPORTED";
-	case VK_ERROR_FRAGMENTED_POOL: return "VK_ERROR_FRAGMENTED_POOL";
-	case VK_ERROR_UNKNOWN: return "VK_ERROR_UNKNOWN";
-	case VK_ERROR_OUT_OF_POOL_MEMORY: return "VK_ERROR_OUT_OF_POOL_MEMORY";
-	case VK_ERROR_INVALID_EXTERNAL_HANDLE: return "VK_ERROR_INVALID_EXTERNAL_HANDLE";
-	case VK_ERROR_FRAGMENTATION: return "VK_ERROR_FRAGMENTATION";
-	case VK_ERROR_INVALID_OPAQUE_CAPTURE_ADDRESS: return "VK_ERROR_INVALID_OPAQUE_CAPTURE_ADDRESS";
-	case VK_ERROR_SURFACE_LOST_KHR: return "VK_ERROR_SURFACE_LOST_KHR";
-	case VK_ERROR_NATIVE_WINDOW_IN_USE_KHR: return "VK_ERROR_NATIVE_WINDOW_IN_USE_KHR";
-	case VK_ERROR_OUT_OF_DATE_KHR: return "VK_ERROR_OUT_OF_DATE_KHR";
-	case VK_ERROR_INCOMPATIBLE_DISPLAY_KHR: return "VK_ERROR_INCOMPATIBLE_DISPLAY_KHR";
-	case VK_ERROR_VALIDATION_FAILED_EXT: return "VK_ERROR_VALIDATION_FAILED_EXT";
-	case VK_ERROR_INVALID_SHADER_NV: return "VK_ERROR_INVALID_SHADER_NV";
-	case VK_ERROR_INVALID_DRM_FORMAT_MODIFIER_PLANE_LAYOUT_EXT:
-		return "VK_ERROR_INVALID_DRM_FORMAT_MODIFIER_PLANE_LAYOUT_EXT";
-	case VK_ERROR_NOT_PERMITTED_EXT: return "VK_ERROR_NOT_PERMITTED_EXT";
-	case VK_ERROR_FULL_SCREEN_EXCLUSIVE_MODE_LOST_EXT: return "VK_ERROR_FULL_SCREEN_EXCLUSIVE_MODE_LOST_EXT";
-	default: return "[UNKNOWN]";
+	CASE(VK_ERROR_OUT_OF_HOST_MEMORY);
+	CASE(VK_ERROR_OUT_OF_DEVICE_MEMORY);
+	CASE(VK_ERROR_INITIALIZATION_FAILED);
+	CASE(VK_ERROR_DEVICE_LOST);
+	CASE(VK_ERROR_MEMORY_MAP_FAILED);
+	CASE(VK_ERROR_LAYER_NOT_PRESENT);
+	CASE(VK_ERROR_EXTENSION_NOT_PRESENT);
+	CASE(VK_ERROR_FEATURE_NOT_PRESENT);
+	CASE(VK_ERROR_INCOMPATIBLE_DRIVER);
+	CASE(VK_ERROR_TOO_MANY_OBJECTS);
+	CASE(VK_ERROR_FORMAT_NOT_SUPPORTED);
+	CASE(VK_ERROR_FRAGMENTED_POOL);
+	CASE(VK_ERROR_UNKNOWN);
+	CASE(VK_ERROR_OUT_OF_POOL_MEMORY);
+	CASE(VK_ERROR_INVALID_EXTERNAL_HANDLE);
+	CASE(VK_ERROR_FRAGMENTATION);
+	CASE(VK_ERROR_INVALID_OPAQUE_CAPTURE_ADDRESS);
+	CASE(VK_ERROR_SURFACE_LOST_KHR);
+	CASE(VK_ERROR_NATIVE_WINDOW_IN_USE_KHR);
+	CASE(VK_ERROR_OUT_OF_DATE_KHR);
+	CASE(VK_ERROR_INCOMPATIBLE_DISPLAY_KHR);
+	CASE(VK_ERROR_VALIDATION_FAILED_EXT);
+	CASE(VK_ERROR_INVALID_SHADER_NV);
+	CASE(VK_ERROR_INVALID_DRM_FORMAT_MODIFIER_PLANE_LAYOUT_EXT);
+	CASE(VK_ERROR_NOT_PERMITTED_KHR);
+	CASE(VK_ERROR_FULL_SCREEN_EXCLUSIVE_MODE_LOST_EXT);
+	CASE(VK_ERROR_COMPRESSION_EXHAUSTED_EXT);
+	// Just to satisfy -Wswitch
+	CASE(VK_RESULT_MAX_ENUM);
+	// No `default` to make `-Werror -Wswitch` protection work
 	}
+#undef CASE
 }
 
-const char *VulkanUtils::getVkResultDescription(VkResult result) noexcept
+std::string_view VulkanUtils::getVkFormatString(VkFormat format) noexcept
 {
-	// Copy-pasted from Vulkan spec 1.2.132
-	switch (result) {
-	// Result codes
-	case VK_SUCCESS: return "Command successfully completed";
-	case VK_NOT_READY: return "A fence or query has not yet completed";
-	case VK_TIMEOUT: return "A wait operation has not completed in the specified time";
-	case VK_EVENT_SET: return "An event is signaled";
-	case VK_EVENT_RESET: return "An event is unsignaled";
-	case VK_INCOMPLETE: return "A return array was too small for the result";
-	case VK_SUBOPTIMAL_KHR:
-		return "A swapchain no longer matches the surface properties exactly, "
-		       "but can still be used to present to the surface successfully";
-	// Error codes
-	case VK_ERROR_OUT_OF_HOST_MEMORY: return "A host memory allocation has failed";
-	case VK_ERROR_OUT_OF_DEVICE_MEMORY: return "A device memory allocation has failed";
-	case VK_ERROR_INITIALIZATION_FAILED:
-		return "Initialization of an object could not be completed for implementation-specific reasons";
-	case VK_ERROR_DEVICE_LOST: return "The logical or physical device has been lost";
-	case VK_ERROR_MEMORY_MAP_FAILED: return "Mapping of a memory object has failed";
-	case VK_ERROR_LAYER_NOT_PRESENT: return "A requested layer is not present or could not be loaded";
-	case VK_ERROR_EXTENSION_NOT_PRESENT: return "A requested extension is not supported";
-	case VK_ERROR_FEATURE_NOT_PRESENT: return "A requested feature is not supported";
-	case VK_ERROR_INCOMPATIBLE_DRIVER:
-		return "The requested version of Vulkan is not supported by the driver "
-		       "or is otherwise incompatible for implementation-specific reasons";
-	case VK_ERROR_TOO_MANY_OBJECTS: return "Too many objects of the type have already been created";
-	case VK_ERROR_FORMAT_NOT_SUPPORTED: return "A requested format is not supported on this device";
-	case VK_ERROR_FRAGMENTED_POOL: return "A pool allocation has failed due to fragmentation of the pool’s memory";
-	case VK_ERROR_UNKNOWN:
-		return "An unknown error has occurred; either the application "
-		       "has provided invalid input, or an implementation failure has occurred";
-	case VK_ERROR_OUT_OF_POOL_MEMORY: return "A pool memory allocation has failed";
-	case VK_ERROR_INVALID_EXTERNAL_HANDLE: return "An external handle is not a valid handle of the specified type";
-	case VK_ERROR_FRAGMENTATION: return "A descriptor pool creation has failed due to fragmentation";
-	case VK_ERROR_INVALID_OPAQUE_CAPTURE_ADDRESS:
-		return "A buffer creation or memory allocation failed because the requested address is not available";
-	case VK_ERROR_SURFACE_LOST_KHR: return "A surface is no longer available";
-	case VK_ERROR_NATIVE_WINDOW_IN_USE_KHR:
-		return "The requested window is already in use by Vulkan "
-		       "or another API in a manner which prevents it from being used again";
-	case VK_ERROR_OUT_OF_DATE_KHR:
-		return "A surface has changed in such a way that it is no longer compatible with "
-		       "the swapchain, and further presentation requests using the swapchain will fail";
-	case VK_ERROR_INCOMPATIBLE_DISPLAY_KHR:
-		return "The display used by a swapchain does not use the same presentable "
-		       "image layout, or is incompatible in a way that prevents sharing an image";
-	case VK_ERROR_VALIDATION_FAILED_EXT: return "No description in spec";
-	case VK_ERROR_INVALID_SHADER_NV: return "One or more shaders failed to compile or link";
-	case VK_ERROR_INVALID_DRM_FORMAT_MODIFIER_PLANE_LAYOUT_EXT:
-		return "No description in spec";
-	case VK_ERROR_NOT_PERMITTED_EXT: return "The caller does not have sufficient privileges";
-	case VK_ERROR_FULL_SCREEN_EXCLUSIVE_MODE_LOST_EXT:
-		return "An operation on a swapchain created with VK_FULL_SCREEN_EXCLUSIVE_APPLICATION_CONTROLLED_EXT "
-		       "failed as it did not have exlusive full-screen access";
-	default: return "Unknown or wrong VkResult value";
-	}
-}
+	using namespace std::string_view_literals;
 
-const char *VulkanUtils::getVkFormatString(VkFormat format) noexcept
-{
 	// The list is copy-pasted from Vulkan 1.2.170 headers.
 	// Multiplane, some block, 4-bit, scaled and other crazy formats were removed.
-#define FORMAT_ENTRY(fmt) case fmt: return #fmt
+#define FORMAT_ENTRY(fmt) case fmt: return #fmt##sv
 	switch (format) {
 	FORMAT_ENTRY(VK_FORMAT_UNDEFINED);
 	FORMAT_ENTRY(VK_FORMAT_R8_UNORM);
@@ -358,7 +309,7 @@ const char *VulkanUtils::getVkFormatString(VkFormat format) noexcept
 	FORMAT_ENTRY(VK_FORMAT_BC6H_SFLOAT_BLOCK);
 	FORMAT_ENTRY(VK_FORMAT_BC7_UNORM_BLOCK);
 	FORMAT_ENTRY(VK_FORMAT_BC7_SRGB_BLOCK);
-	default: return "Unknown or wrong VkFormat value";
+	default: return "VK_FORMAT_[UNKNOWN]"sv;
 	}
 #undef FORMAT_ENTRY
 }
