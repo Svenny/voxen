e680655a2cee5de98ebda6781ee55eba023d7e24
Pavel Asyutchenko <sventeam@yandex.ru>
2023-03-20 22:51:41 +0300

    [cmake] Make FMT library shared
    Going to be often used by non-libvoxen targets too.

--- 3rdparty/fmt.cmake
+++ 3rdparty/fmt.cmake
@@ -1,5 +1,6 @@
-voxen_add_library(fmt STATIC)
+voxen_add_library(fmt SHARED)
 add_library(3rdparty::fmt ALIAS fmt)
 
+target_compile_definitions(fmt PRIVATE FMT_EXPORT)
 target_include_directories(fmt PUBLIC fmt/include)
 target_sources(fmt PRIVATE fmt/src/format.cc fmt/src/os.cc)
