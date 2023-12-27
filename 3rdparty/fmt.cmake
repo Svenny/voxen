a7ff60915a17ceeb7faa40823c2885be47b42c99
Pavel Asyutchenko <svenpavel@gmail.com>
2023-12-28 00:14:02 +0100

    [3rdparty] Update fmt to 10.1.1

--- 3rdparty/fmt.cmake
+++ 3rdparty/fmt.cmake
@@ -1,6 +1,8 @@
 voxen_add_library(fmt SHARED)
 add_library(3rdparty::fmt ALIAS fmt)
 
-target_compile_definitions(fmt PRIVATE FMT_EXPORT)
-target_include_directories(fmt PUBLIC fmt/include)
+target_compile_definitions(fmt PRIVATE FMT_LIB_EXPORT)
+# Interface header of this library violates -Wdeprecated
+# Mark it as system header to avoid failures in user code
+target_include_directories(fmt SYSTEM PUBLIC fmt/include)
 target_sources(fmt PRIVATE fmt/src/format.cc fmt/src/os.cc)
