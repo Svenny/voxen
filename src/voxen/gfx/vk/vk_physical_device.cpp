396e79339872c85465fb3393a3fab79bd03114a4
Pavel Asyutchenko <svenpavel@gmail.com>
2024-06-18 22:57:41 +0200

    [gfx] Add VK_EXT_memory_budget discovery

--- src/voxen/gfx/vk/vk_physical_device.cpp
+++ src/voxen/gfx/vk/vk_physical_device.cpp
@@ -105,6 +105,8 @@ std::pair<void *, void *> PhysicalDevice::prepareExtInfoQuery(const Instance &in
 
 			m_ext_info.props_maintenance5.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_MAINTENANCE_5_PROPERTIES_KHR;
 			m_ext_info.props_maintenance5.pNext = std::exchange(props_pnext_last, &m_ext_info.props_maintenance5);
+		} else if (!strcmp(ext.extensionName, VK_EXT_MEMORY_BUDGET_EXTENSION_NAME)) {
+			m_ext_info.have_memory_budget = true;
 		} else if (!strcmp(ext.extensionName, VK_EXT_MESH_SHADER_EXTENSION_NAME)) {
 			m_ext_info.have_mesh_shader = true;
 
