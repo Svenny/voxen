b34e6d4e49595017d062b10240257e1d33f417e1
Pavel Asyutchenko <svenpavel@gmail.com>
2024-10-18 00:45:53 +0200

    [debug] Stub-out demangle code on windows
    The last linux-only part is now fixed.

--- src/util/debug.cpp
+++ src/util/debug.cpp
@@ -1,13 +1,20 @@
 #include <voxen/util/debug.hpp>
 
-#include <cxxabi.h>
+#ifndef _WIN32
+	#include <cxxabi.h>
+#endif
 
 namespace voxen
 {
 
 DebugUtils::DemanglePtr DebugUtils::demangle(const char *name) noexcept
 {
+#ifndef _WIN32
 	char *ptr = abi::__cxa_demangle(name, nullptr, nullptr, nullptr);
+#else
+	// TODO (Svenny): add demangling for windows
+	char *ptr = strdup(name);
+#endif
 	return DemanglePtr(ptr, &std::free);
 }
 
