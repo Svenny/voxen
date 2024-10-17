5b2eed5495543ebdded1ae6e59c7954dddf216dc
Pavel Asyutchenko <svenpavel@gmail.com>
2024-10-18 00:37:42 +0200

    [os] Add stdlib platform wrapper code

---
+++ include/voxen/os/stdlib.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstdio>
+
+namespace voxen::os
+{
+
+// Various wrappers that hide some shittiness of C standard library
+namespace Stdlib
+{
+
+// Get file size in bytes.
+// Returns negative value (likely setting errno) upon error.
+int64_t fileSize(FILE *stream);
+
+} // namespace Stdlib
+
+} // namespace voxen::os
